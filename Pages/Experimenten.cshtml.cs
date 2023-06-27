f5119438caff6a3f8a85367c08a9211956b63987
TomSwennen <tswennen@gmail.com>
2023-06-27 16:50:34 +0200

    Initial Commit

---
+++ Pages/Experimenten.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Klooz3.Pages
+{
+    public class ExperimentenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Over.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Klooz3.Pages
+{
+    public class OverModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
