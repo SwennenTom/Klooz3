118c470ba160dcb7d1a85637959a351f453d060f
TomSwennen <tswennen@gmail.com>
2023-07-04 12:40:35 +0200

    switch 2 views & fixed hyperlinx

---
+++ Controllers/ExperimentenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Klooz3.Controllers
+{
+    public class ExperimentenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
