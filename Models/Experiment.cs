e6a427d304793a581cc3e00ca267927dbb1cf803
TomSwennen <tswennen@gmail.com>
2023-07-24 14:00:14 +0200

    feedback verwerkt

--- Models/Experiment.cs
+++ Models/Experiment.cs
@@ -13,29 +13,53 @@
         public Categories? categories { get; set; }
 
         public string? experimentShortText { get; set; }
-        public string? experimentPartners { get; set; }
+        public Partner? experimentPartners { get; set; }
         public DateTime? experimentKickOffDate { get; set; }
+        public DateTime? experimentEndDate { get; set; }
         public string? experimentwickedProblemsToSmartSolutions { get; set; }
         public string? experimenttargetAndImpact { get; set; }
         public string? experimentTouchstone { get; set; }
         public byte[]? experimentPhotos { get; set; }
         public bool? experimentPublished { get; set; }
 
-        public Experiment(byte[]? experimentImage, string? experimentName, string? experimentCardFrontText, string? experimentCardBackText, int? categoriesId, string? experimentShortText, string? experimentPartners, DateTime? experimentKickOffDate, string? experimentwickedProblemsToSmartSolutions, string? experimenttargetAndImpact, string? experimentTouchstone, byte[]? experimentPhotos, bool? experimentPublished)
+        public User? experimentOwner { get; set; }
+        public DateTime? experimentCreatedDate { get; set; }
+        public DateTime? experimentLastModified { get; set; }
+        public User? experimentLastModifiedBy { get; set; }
+        public ExperimentStatus? experimentStatus { get; set; }
+
+        public enum ExperimentStatus
         {
+            Ingediend,
+            Bezig,
+            Klaar,
+            Lopende,
+            Gearchiveerd
+        }
+
+        public Experiment(int experimentId, byte[]? experimentImage, string? experimentName, string? experimentCardFrontText, string? experimentCardBackText, int? categoriesId, Categories? categories, string? experimentShortText, Partner? experimentPartners, DateTime? experimentKickOffDate, DateTime? experimentEndDate, string? experimentwickedProblemsToSmartSolutions, string? experimenttargetAndImpact, string? experimentTouchstone, byte[]? experimentPhotos, bool? experimentPublished, User? experimentOwner, DateTime? experimentCreatedDate, DateTime? experimentLastModified, User? experimentLastModifiedBy, ExperimentStatus? experimentStatus)
+        {
+            this.experimentId = experimentId;
             this.experimentImage = experimentImage;
             this.experimentName = experimentName;
             this.experimentCardFrontText = experimentCardFrontText;
             this.experimentCardBackText = experimentCardBackText;
             this.categoriesId = categoriesId;
+            this.categories = categories;
             this.experimentShortText = experimentShortText;
             this.experimentPartners = experimentPartners;
             this.experimentKickOffDate = experimentKickOffDate;
+            this.experimentEndDate = experimentEndDate;
             this.experimentwickedProblemsToSmartSolutions = experimentwickedProblemsToSmartSolutions;
             this.experimenttargetAndImpact = experimenttargetAndImpact;
             this.experimentTouchstone = experimentTouchstone;
             this.experimentPhotos = experimentPhotos;
             this.experimentPublished = experimentPublished;
+            this.experimentOwner = experimentOwner;
+            this.experimentCreatedDate = experimentCreatedDate;
+            this.experimentLastModified = experimentLastModified;
+            this.experimentLastModifiedBy = experimentLastModifiedBy;
+            this.experimentStatus = experimentStatus;
         }
 
         public Experiment()
---
+++ Models/User.cs
@@ -0,0 +1,36 @@
+﻿using Klooz3.Data;
+
+namespace Klooz3.Models
+{
+    public class User
+    {
+        public int? userId {  get; set; }
+        public string? userEmail { get; set; }
+        public string? userVoornaam { get; set; }
+        public string? userAchternaam { get; set; }
+        public string? userAdressLine1 { get; set; }
+        public string? userPostcode { get; set; }
+        public string? userGemeente { get; set; }
+        public string? userPhoneNumber { get; set; }
+        public Roles? userrole { get; set; }
+        public DateTime? userJoined { get; set; }
+        public bool? userIsAccountActive { get; set; }
+
+        public User(int? userId, string? userEmail, string? userVoornaam, string? userAchternaam, string? userAdressLine1, string? userPostcode, string? userGemeente, string? userPhoneNumber, Roles? userrole, DateTime? userJoined, bool? userIsAccountActive)
+        {
+            this.userId = userId;
+            this.userEmail = userEmail;
+            this.userVoornaam = userVoornaam;
+            this.userAchternaam = userAchternaam;
+            this.userAdressLine1 = userAdressLine1;
+            this.userPostcode = userPostcode;
+            this.userGemeente = userGemeente;
+            this.userPhoneNumber = userPhoneNumber;
+            this.userrole = userrole;
+            this.userJoined = userJoined;
+            this.userIsAccountActive = userIsAccountActive;
+        }
+
+        public User() { }
+    }
+}
