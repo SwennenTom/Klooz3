b16623515dc6742aaad099c0bfc206afc43c923a
TomSwennen <tswennen@gmail.com>
2023-11-06 11:10:28 +0100

    User Roles are being shown in list

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,22 +1,41 @@
 ﻿using Klooz3.Data;
+using Klooz3.Models;
+using Klooz3.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Klooz3.Controllers
 {
     public class UsersController : Controller
     {
         private readonly UserService _userService;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UsersController(UserService userService)
+        public UsersController(UserService userService, UserManager<IdentityUser> userManager)
         {
             _userService = userService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var users = await _userService.GetRegisteredUsersAsync();
-            return View(users);
+            var usersWithRoles = new List<UserWithRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                usersWithRoles.Add(new UserWithRolesViewModel
+                {
+                    User = user,
+                    Roles = roles
+                });
+            }
+
+            return View(usersWithRoles);
         }
+
+        
     }
 }
---
+++ ViewModels/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Klooz3.ViewModels
+{
+    public class UserWithRolesViewModel
+    {
+        public IdentityUser User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
