f401cf4cce743908e74536eaf501253152233d8e
TomSwennen <tswennen@gmail.com>
2023-11-06 11:41:06 +0100

    RolesAreChangable

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,6 +4,9 @@ using Klooz3.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Klooz3.Controllers
 {
@@ -11,11 +14,13 @@ namespace Klooz3.Controllers
     {
         private readonly UserService _userService;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(UserService userService, UserManager<IdentityUser> userManager)
+        public UsersController(UserService userService, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userService = userService;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task<IActionResult> Index()
@@ -36,6 +41,49 @@ namespace Klooz3.Controllers
             return View(usersWithRoles);
         }
 
-        
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            // Query the available roles using the RoleManager or any other method
+            var availableRoles = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+
+            var model = new EditUserRolesViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                SelectedRole = roles.FirstOrDefault(),
+                AvailableRoles = availableRoles // Populate AvailableRoles with available roles
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, EditUserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Update user's roles based on the model.SelectedRoles
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var roleToAdd = model.SelectedRole;
+            var rolesToRemove = userRoles.Except(new[] { roleToAdd });
+
+            await _userManager.AddToRoleAsync(user, roleToAdd);
+            await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
+
+            return RedirectToAction("Index"); // Redirect to the user list
+        }
     }
 }
---
+++ ViewModels/EditUserRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Klooz3.ViewModels
+{
+    public class EditUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<string> AvailableRoles { get; set; }
+        public string SelectedRole { get; set; }
+    }
+}
