f1524603d48a6df1ad1148211609cc06b07e25e7
TomSwennen <tswennen@gmail.com>
2023-11-20 14:01:32 +0100

    added expform view and controller

---
+++ Controllers/ExperimentenFormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Klooz3.Controllers
+{
+    public class ExperimentenFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
