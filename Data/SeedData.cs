9f5270cfb0e3e12c7af8a38370fddc599ea33861
TomSwennen <tswennen@gmail.com>
2023-12-14 13:35:43 +0100

    update Seeddata

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -36,9 +36,39 @@ namespace Klooz3.Data
                 var _userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
                 var _roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
                 await VoegRollenToeAsync(_context, _roleManager);
+                await CreateIdentityUserAsync(_userManager);
                 //await CreateIdentityRecordAsync(Roles.regierol, "student@pxl.be", "Student1!", Roles.regierol);
                 //await CreateIdentityRecordAsync(Roles.gebruikerrol, "lector@pxl.be", "Lector1!", Roles.gebruikerrol);
                 //await CreateIdentityRecordAsync(Roles.adminrol, "admin@pxl.be", "Admin1!", Roles.adminrol);
+            }
+        }
+
+        private static async Task CreateIdentityUserAsync(UserManager<ApplicationUser> _userManager)
+        {
+            string firstname = "Tom";
+            string lastname = "Swennen";
+            string phonenumber = "0498147788";
+            string email = "tswennen@gmail.com";
+            string organization = "nvt";
+            string password = "O826o826";
+
+            if (_userManager != null && await _userManager.FindByEmailAsync(email) == null)
+            {
+                var identityUser = new ApplicationUser() { Firstname = firstname,Lastname=lastname,PhoneNumber=phonenumber,Organization=organization, Email = email };
+                var result = await _userManager.CreateAsync(identityUser, password);
+                if (result.Succeeded)
+                {
+                    // Add the user to a role if needed
+                    await _userManager.AddToRoleAsync(identityUser, "adminrol");
+
+                    // You can customize this part based on your needs
+                    Console.WriteLine("User created successfully!");
+                }
+                else
+                {
+                    // Handle errors if user creation fails
+                    Console.WriteLine("Error creating user: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+                }
             }
         }
 
